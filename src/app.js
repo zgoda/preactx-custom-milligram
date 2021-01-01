4bebaacb47cbf51000d5d4e7e4671584d8c5b9e5
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-01-01 20:59:56 +0100

    Minimise the example

--- src/app.js
+++ src/app.js
@@ -1,32 +1,10 @@
-import { Component } from 'preact';
-import { Router } from 'preact-router';
+const App = (() => {
+  return (
+    <section class="container">
+      <h1>Welcome to my site!</h1>
+      <p>Customised Milligram.css is being used here!</p>
+    </section>
+  )
+});
 
-import Header from './header';
-
-// Code-splitting is automated for routes
-import Home from './views/home';
-import Profile from './views/profile';
-
-export default class App extends Component {
-	
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
-
-	render() {
-		return (
-			<div id="app">
-				<Header />
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-				</Router>
-			</div>
-		);
-	}
-}
+export { App };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './style';
-import App from './app';
+import { App } from './app';
 
 export default App;
